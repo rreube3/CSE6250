ff076ce2080c641001bc7640ed1b2cd25d978ebf
rreube3 <93461855+rreube3@users.noreply.github.com>
2022-04-21 20:03:33 -0400

    Update read_numberbatch_en_data.py

--- read_numberbatch_en_data.py
+++ read_numberbatch_en_data.py
@@ -3,12 +3,9 @@ import gzip
 import shutil
 import os
 
-path="/data"
-os.chdir(path)
-
 import gzip
 import shutil
 f_in=open
-with gzip.open('numberbatch-en-19.08.txt.gz', 'rb') as f_in:
-    with open('numberbatch-en.txt','wb') as f_out:
+with gzip.open('data/numberbatch-en-19.08.txt.gz', 'rb') as f_in:
+    with open('data/numberbatch-en.txt','wb') as f_out:
         shutil.copyfileobj(f_in,f_out)
